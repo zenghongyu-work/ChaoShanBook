f47300df1bb228a2fe7f23ff4588d554aa4d041c
zenghongyu-work <86586131a>
2020-01-12 20:05:46 +0800

    提交

--- src/main/resources/databaseSql/createArticlePictureTable.sql
+++ src/main/resources/databaseSql/createArticlePictureTable.sql
@@ -2,4 +2,4 @@ CREATE TABLE `article_picture` (
   `id`   INT PRIMARY KEY AUTO_INCREMENT,
   `article_id` INT not null,
   `path`   VARCHAR(64) not null
-) COMMENT='文章' engine=InnoDB default charset=utf8;
+) COMMENT='文章图片' engine=InnoDB default charset=utf8;
